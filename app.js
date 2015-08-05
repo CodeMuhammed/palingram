fdf17db7f28a7f790dd80f1bc8266c97c0ee8cf7
codemuhammed <codemuhammed@gmail.com>
2015-08-05 07:13:32 -0700

    dsd

--- app.js
+++ app.js
@@ -1,81 +1,60 @@
-//Require dependencies phase
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var errorHandler = require('errorhandler');
-var logger = require('morgan');
-var compression = require('compression');
-var methodOverride = require('method-override');
-var favicon = require('serve-favicon');
-var busboy = require('connect-busboy');
-var passport  = require('passport');
-var cors  = require('cors');
 
-var socket = require('socket.io');
-var http  = require ('http');
-var fs = require('fs');
-var path = require('path');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-
-//Instantiate a new express app
 var app = express();
 
-//set the model source @TODO use app.set(app.use) here ., Then return a function
-//that returns the object
-var dbResource = require('./app_server/models/dbResource')('restapi' , {} , app);
-//initialize database
-dbResource.initColls(function(){
-	//initialize passport
-	var initPassport = require('./app_server/controllers/passportCtrl');
-	initPassport(passport , dbResource);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-	//Configure the express app
-	app.set('busboy' , busboy);
-	app.set('port' , process.env.PORT || 3000);
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    //cors 
-	app.use(cors({credentials: true, origin: true}));
-	
-	app.use(compression({threshold:1}));
-	//app.use(logger('combined'));
-	app.use(methodOverride('_method'));
+app.use('/', routes);
+app.use('/users', users);
 
-	//configure router to use cookie-parser  ,body-parser 
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({extended:true}));
-	app.use(cookieParser());
-	app.use(session({resave:true , secret:'this string' , saveUninitialized:true}));
-
-	//use passport local strategy for login , logout and signup 
-	app.use(passport.initialize());
-	app.use(passport.session());
-	
-	app.use(express.static(path.join(__dirname , 'public')));;
-	
-	//Define routes for authentication
-	app.use('/auth' , require('./routes/authenticate')(passport));
-
-	//api routes starts here
-	app.use('/api' , require('./routes/api')(dbResource));
-
-	//Define routes and middle wares in a separate module
-	//require('./routes')(app , dbResource);
-    app.use(express.static(path.join(__dirname , 'public')));;
-	//handle errors using custom or 3rd party middle-wares
-	app.use(errorHandler());
-
-	//Start the app
-	var server  = http.createServer(app);
-	var io = socket.listen(server);
-
-	io.sockets.on('connection' , function(socket){
-		socket.on('messageChange' , function(data){
-		});
-		socket.emit('hey' , {msg:'hello'});
-	});
-
-	server.listen(app.get('port') , function(){
-		console.log('Server running on port ' ,app.get('port'));
-	});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
