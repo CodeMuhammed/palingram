234044b1b362f6ea0e494c4df816f7ed67e72d7d
codemuhammed <codemuhammed@gmail.com>
2015-08-11 04:15:45 -0700

    testing google crawlability
    Signed-off-by: codemuhammed <codemuhammed@gmail.com>

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var favicon = require('serve-favicon');
 var path = require('path');
 
 var request = require('request');
-var palingramapi = 'http://palingramapi.herokuapp.com/api/allPosts';
+var palingramapi = 'http://palingramapi.herokuapp.com/api';
+//var palingramapi = 'http://localhost:3000/api/allPosts';
 
 //Instantiate a new express app
 var app = express();
@@ -25,25 +26,35 @@ app.set('view engine' , 'ejs');
 app.set('port' , process.env.PORT || 3002);
 
 app.use(compression({threshold:1}));
-app.use(logger('combined'));
 app.use(methodOverride('_method'));
 app.use(favicon(path.join(__dirname , 'public', 'favicon.ico')));
 
 //configure router to use cookie-parser  ,body-parser 
 app.get('/' , function(req , res){  
-	 request.get(palingramapi)
-	    .on('response' , function(response){
-              res.status(200).send(response);
-	    })
-	    .on('error' , function(err){
-             res.status(500).send(err);
-	    });
+	 request.get(palingramapi+'/allPosts' , function(err , response , body){
+         if(err){
+              res.status(500).send(err);
+         } 
+         else {
+         	res.render('index.ejs' , {posts:JSON.parse(body)});
+         }
+	 });
     
 });
 
 //configure router to use cookie-parser  ,body-parser 
 app.get('/preview/:post_id' , function(req , res){  
-     res.render('post.ejs' , {post_id  : req.params.post_id});
+	 console.log(req.params.post_id);
+	 request.get(palingramapi+'/posts/'+req.params.post_id , function(err , response , body){
+         if(err){
+              res.status(500).send(err);
+         } 
+         else {
+         	var post = JSON.parse(body)[0];
+         	console.log(post.body);
+         	res.render('post.ejs' , {post:post});
+         }
+	 });
 });
 
 app.use(bodyParser.json());
