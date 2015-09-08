1c6fe2f73b37658c845803fc8c25c00454d90fcd
codemuhammed <codemuhammed@gmail.com>
2015-09-07 21:15:27 -0700

    sdfghjkl

--- public/js/services/Auth.js
+++ public/js/services/Auth.js
@@ -1,6 +1,6 @@
 angular.module('Auth' , ['User', 'Tags' , 'Posts'])
-    //.constant('BaseUrl' , 'http://localhost:3000')
-    .constant('BaseUrl' , 'https://palingramapi.herokuapp.com')
+    .constant('BaseUrl' , 'http://localhost:3000')
+    //.constant('BaseUrl' , 'https://palingramapi.herokuapp.com')
     .constant('googl_key' , 'AIzaSyBFgXT17fbaEr-POzSmbLzdzMGlKEoUH44')
    .factory('Auth' , function($http , $q ,$state ,$rootScope ,  Posts , Tags , User , BaseUrl , googl_key){
          var isSignedIn;
