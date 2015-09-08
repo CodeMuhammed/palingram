646505f247d05c5f21abf1704e8df9fb2dbeef5d
codemuhammed <codemuhammed@gmail.com>
2015-09-08 02:13:31 -0700

    dtyuiop

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -1079,4 +1079,10 @@ angular.module('palingram')
             return promise.promise;
         };
 
+
+
+        var fblinks = "SunCoastMLM/,423224154383506/,182333858497701/,136743439694235/,postyourbusiness/,pinoyonlinebusinessgroup/" ;
+
+          $scope.links = fblinks.split(',');
+
    });
