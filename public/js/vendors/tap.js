97cc296b3d1dee639f41daa846118fbdbab20aac
codemuhammed <codemuhammed@gmail.com>
2016-04-11 21:30:10 +0100

    Signed-off-by: codemuhammed <codemuhammed@gmail.com>

---
+++ public/js/vendors/tap.js
@@ -0,0 +1,7 @@
+var tap = function(data){
+	console.log('Tap loading ....... %s', data);
+}
+/*var iframeBody = $('body', $('#iframe')[0].contentWindow.document);
+	$(iframeBody).on('click', 'a', function(event) {
+		alert(event);
+	});*/
